cfed51ba56256b5c8eae366a5f637a3abd2f07f2
インターンユーザ02 <200020@matsusaka.co.jp>
2020-10-29 13:49:49 +0900

    commit-4

--- quiz/settings.py
+++ quiz/settings.py
@@ -122,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
